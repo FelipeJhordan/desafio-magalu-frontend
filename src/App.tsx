b658ce613e5ddc813133761b32bc77868b86968c
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-28 19:42:01 -0300

    feat: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, useRoutes } from 'react-router-dom';
+import './App.css';
+import LoginPage from './pages/login.page';
+import { AuthProvider } from './hooks/useAuth';
+import ProtectedLayout from './components/layout/protected-layout.component';
+import UploadFilePage from './pages/upload-file';
+import ListOrderByUserPage from './pages/list-orders-by-user.page';
 
-import './App.css'
 
 function App() {
-  return (
-    <>
-      OSStation
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+      <Route element={<AuthProvider />}>
+        <Route element={<ProtectedLayout />} >
+          <Route path="/upload-file" element={<UploadFilePage />} />
+          <Route path="/" element={<ListOrderByUserPage />} />
+        </Route>
+  
+         <Route path="/login" element={<LoginPage />} />
+        </Route>
+         
+      </>
+    )
   );
+
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
-export default App
+export default App;

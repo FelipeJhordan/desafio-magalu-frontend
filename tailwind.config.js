938bebb9435fc04c5cbecabbb322d9171b7c3f02
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-28 08:14:21 -0300

    feat: set default theme with a default color/background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00FFA3',
+        secondary: '#ffed4a',
+        danger: '#e3342f',
+      },
+      backgroundColor: {
+        default: '#2B3C51', 
+        navbar: '#background: #223041',
+        input: '#223041',
+        default_strong: '#1D2938',
+      },
+    },
   },
   plugins: [],
 }

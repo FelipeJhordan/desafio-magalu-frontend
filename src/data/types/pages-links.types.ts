d3bca16bd740dca9598628cff89d8d8e06d181fd
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-29 19:16:59 -0300

    feat: create util type to use in sidebar

---
+++ src/data/types/pages-links.types.ts
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+
+export type PagesChildLinks = {
+    icon?: ReactElement<ReactElement>;
+    name: string;
+    parent?: string;
+    path?: string;
+}
+
+
+export type PagesLinksParentLinks = Omit<PagesChildLinks, 'parent'> & {
+    children: PagesChildLinks[];
+    active: boolean;
+}

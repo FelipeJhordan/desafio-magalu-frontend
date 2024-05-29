02c0ccf331771292fa1fbbeea7b49c5246dbbb9b
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-29 19:13:18 -0300

    feat: add home route and create class to icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, useRoutes } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
 import './App.css';
+
 import LoginPage from './pages/login.page';
 import { AuthProvider } from './hooks/useAuth';
 import ProtectedLayout from './components/layout/protected-layout.component';
 import UploadFilePage from './pages/upload-file';
-import ListOrderByUserPage from './pages/list-orders-by-user.page';
-
+import HomePage from './pages/home.page';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-      <Route element={<AuthProvider />}>
-        <Route element={<ProtectedLayout />} >
-          <Route path="/upload-file" element={<UploadFilePage />} />
-          <Route path="/" element={<ListOrderByUserPage />} />
+        <Route element={<AuthProvider />}>
+          <Route element={<ProtectedLayout />}>
+            <Route path="/upload" element={<UploadFilePage />} />
+            <Route path="/pedidos" element={<UploadFilePage />} />
+            <Route path="/" element={<HomePage />} />
+          </Route>
+
+          <Route path="/login" element={<LoginPage />} />
         </Route>
-  
-         <Route path="/login" element={<LoginPage />} />
-        </Route>
-         
       </>
     )
   );
 
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;

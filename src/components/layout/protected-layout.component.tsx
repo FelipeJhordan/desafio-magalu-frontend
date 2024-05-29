7758df62fff225050eaba53a74334f401856c59d
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-29 19:21:46 -0300

    feat:  setup default layout with sidebar/header

--- src/components/layout/protected-layout.component.tsx
+++ src/components/layout/protected-layout.component.tsx
@@ -1,16 +1,64 @@
+import { Fragment, useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { Transition } from '@headlessui/react';
+
 import { useAuth } from '../../hooks/useAuth';
+import Sidebar from '../sidebar/sidebar.component';
+import Header from '../header/header.component';
 
 const ProtectedLayout = () => {
   const { user } = useAuth();
-  console.log(user)
+
   if (!user) {
     return <Navigate to="/login" />;
   }
+
+  const [showNav, setShowNav] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
+
+  function handleResize() {
+    if (innerWidth <= 640) {
+      setShowNav(false);
+      setIsMobile(true);
+    } else {
+      setShowNav(true);
+      setIsMobile(false);
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window != undefined) {
+      addEventListener('resize', handleResize);
+    }
+
+    return () => {
+      removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <main className="bg-primary">
-        <Outlet />
+      <Header showNav={showNav} setShowNav={setShowNav} />
+      <Transition
+        as={Fragment}
+        show={showNav}
+        enter="transform transition duration-[400ms]"
+        enterFrom="-translate-x-full"
+        enterTo="translate-x-0"
+        leave="transform duration-[400ms] transition ease-in-out"
+        leaveFrom="translate-x-0"
+        leaveTo="-translate-x-full"
+      >
+        <Sidebar showNav={showNav} setShowNav={setShowNav} />
+      </Transition>
+      <main
+        className={`pt-16 transition-all duration-[400ms] ${
+          showNav && !isMobile ? 'pl-56' : ''
+        }`}
+      >
+        <div className="px-4 mt-3 md:px-16">
+          <Outlet />
+        </div>
       </main>
     </>
   );

bece4117789cade90ff4e282bb142d90577e3fb6
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-29 19:11:58 -0300

    feat: update some css theme variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,11 @@ export default {
       },
       backgroundColor: {
         default: '#2B3C51', 
-        navbar: '#background: #223041',
+        header: '#223041',
         input: '#223041',
         default_strong: '#1D2938',
-        switch_bg: '#3D5470'
+        switch_bg: '#3D5470',
+        search_bg: '#F4F4F4'
       },
     },
   },

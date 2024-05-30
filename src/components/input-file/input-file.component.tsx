336a751304af32500ae78170b9143d50b0c41387
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-30 19:06:10 -0300

    feat: create action to send file to back-end

--- src/components/input-file/input-file.component.tsx
+++ src/components/input-file/input-file.component.tsx
@@ -6,10 +6,12 @@ import UploadInputIcon from '../../assets/icons/upload-input.svg';
 import FileIcon from '../../assets/icons/file.svg';
 import { ReactSVG } from 'react-svg';
 import { ToastService } from '../../services/toast/toast.service';
+import Spinner from '../spinner/spinner.component';
 
 type InputFileProps = {
   setFile: (file: File | null) => void;
   file: File | null;
+  isLoading: boolean;
 };
 
 type InternalInputFileProps = {
@@ -19,9 +21,10 @@ type InternalInputFileProps = {
 type HasFileProps = {
   file?: File;
   removeFile: () => void;
+  isLoading: boolean;
 };
 
-export const InputFile = ({ file, setFile }: InputFileProps) => {
+export const InputFile = ({ file, setFile, isLoading }: InputFileProps) => {
   const removeFile = useCallback(() => {
     setFile(null);
   }, [file]);
@@ -33,14 +36,14 @@ export const InputFile = ({ file, setFile }: InputFileProps) => {
   const dropzone = useDropzone({
     onDrop,
     accept: {
-      txt: ['.txt'],
+      'text/plain': ['.txt'],
     },
     multiple: false,
     onError: () => ToastService.alert('Erro ao receber o arquivo', 'error'),
     onDropRejected: () => ToastService.alert('Extensão inválida', 'error'),
   });
 
-  if (file) return <HasFile file={file} removeFile={removeFile} />;
+  if (file) return <HasFile file={file} removeFile={removeFile} isLoading={isLoading} />;
 
   return <InternalInputFile dropzone={dropzone} />;
 };
@@ -80,20 +83,24 @@ const InternalInputFile = ({ dropzone }: InternalInputFileProps) => {
   );
 };
 
-const HasFile = ({ file, removeFile }: HasFileProps) => {
+const HasFile = ({ file, removeFile, isLoading }: HasFileProps) => {
   return (
-    <div className="w-full h-48 rounded-lg border-dashed border-4 border-gray-600 bg-gray-700 flex justify-center items-center">
-      <div className="bg-white w-45 rounded-md shadow-md flex gap-3 items-center justify-center">
-        <ReactSVG src={FileIcon} className="w-5 h-5 my-4 ml-4" />
-        <span className="text-sm text-gray-500 my-4">{file?.name}</span>
-        <button
-          type="button"
-          onClick={removeFile}
-          className="place-self-start mt-1 p-1"
-        >
-          <ReactSVG src={CloseIcon} className="w-5 h-5" />
-        </button>
-      </div>
+    <div className="w-full h-48 rounded-lg border-dashed border-2 border-gray-600 bg-gray-700 flex justify-center items-center">
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="bg-white w-45 rounded-md shadow-md flex gap-3 items-center justify-center">
+          <ReactSVG src={FileIcon} className="w-5 h-5 my-4 ml-4" />
+          <span className="text-sm text-gray-500 my-4">{file?.name}</span>
+          <button
+            type="button"
+            onClick={removeFile}
+            className="place-self-start mt-1 p-1"
+          >
+            <ReactSVG src={CloseIcon} className="w-5 h-5" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/upload-file/upload-file.page.tsx
+++ src/pages/upload-file/upload-file.page.tsx
@@ -6,27 +6,76 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import BreadCrumb from '../../components/breadcrump/breadcrumb.component';
 import { InputFile } from '../../components/input-file/input-file.component';
+import { ToastService } from '../../services/toast/toast.service';
+import { HttpService } from '../../services/http/http.service';
+import { UploadResponseType } from '../../data/sidebar-links/http-response/upload-response.type';
 
 const UploadFilePage: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
 
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = async (e: React.FormEvent) => {
-    console.log(file)
     e.preventDefault();
 
+    const httpService = new HttpService()
+      if (!file) {
+        ToastService.alert('Por favor, selecione um arquivo', 'error');
+        setFile(null);
+        return
+      }
   
-};
+      setLoading(true);
+  
+      const formData = new FormData();
+      formData.append('file', file);
+  
+      const response = await httpService.post<UploadResponseType>(
+        'user-orders/upload',
+        formData,
+        {
+          "Content-Type": "multipart/form-data",
+        }
+      );
+      handleToastMessage(response?.data, response.status);
+  
+      setFile(null);
+      setLoading(false);
+  };
+
+  const handleToastMessage = (
+    response: UploadResponseType,
+    statusCode: number
+  ) => {
+    if (response && response.message === 'SUCCESS') {
+      ToastService.alert('Arquivo processado com sucesso!', 'success');
+      return;
+    }
+
+    if (response && response.message === 'ERROR') {
+      ToastService.alert('Arquivo com processado com alguns erros!', 'warning');
+      return;
+    }
+
+    if (statusCode === 422 || statusCode === 400) {
+      ToastService.alert('Arquivo inválido', 'error');
+      return;
+    }
+
+
+    ToastService.alert('Erro interno no servidor', 'error');
+  };
 
   return (
     <>
       <BreadCrumb current="UPLOAD" parent="HOME" parentPath="/" />
       <ToastContainer className="absolute" />
       <form onSubmit={handleSubmit}>
-        <InputFile setFile={setFile} file={file} />
+        <InputFile setFile={setFile} file={file} isLoading={loading} />
         <div className="w-full mt-5 flex justify-end">
           <button
             className="w-60 bg-green-400 hover:bg-green-500 text-gray-800 font-bold py-3 px-4 rounded-lg"
-            type="submit" 
+            type="submit"
           >
             ENVIAR
           </button>

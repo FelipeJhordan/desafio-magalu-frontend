928f89e6b796f2db855d982efc70f9a2b080cb07
FelipeJhordan <felipejordan.alves@gmail.com>
2024-05-31 08:49:06 -0300

    feat: add get call in http service

--- src/services/http/http.service.ts
+++ src/services/http/http.service.ts
@@ -1,25 +1,22 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 export class HttpService {
+  baseUrl: string;
+  instance: AxiosInstance;
+  constructor(partialUrl = '/api/v1/') {
+    this.baseUrl = import.meta.env.VITE_API_HOST + partialUrl;
+    this.instance = axios.create({ baseURL: this.baseUrl });
+  }
 
-    baseUrl: string
-    instance: AxiosInstance
-    constructor(partialUrl = '/api/v1/') {
-        this.baseUrl =  import.meta.env.VITE_API_HOST + partialUrl;
-        this.instance = axios.create({ baseURL: this.baseUrl });
+  get<T>(url: string, customHeaders = {}): Promise<AxiosResponse<T>> {
+    return this.instance.get(url, customHeaders);
+  }
 
-    }
-
-
-
-    get(url: string, customHeaders = {}) {
-        // return this.request('get', url, null, customHeaders);
-    }
-    
-    post<T>(url: string, data: any, customHeaders = {}): Promise<AxiosResponse<T>> {
-        return this.instance.post(url, data, { headers: customHeaders });
-    }
-    
-   
+  post<T>(
+    url: string,
+    data: any,
+    customHeaders = {}
+  ): Promise<AxiosResponse<T>> {
+    return this.instance.post(url, data, { headers: customHeaders });
+  }
 }
-
